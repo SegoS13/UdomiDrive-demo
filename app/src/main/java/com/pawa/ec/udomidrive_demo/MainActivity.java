88b40ce6f81a28506c3160eb03d7fbc029c66eb6
Mauricio <segoviamz@hotmail.com>
2020-11-18 17:33:24 -0500

    Update Project 30%

--- app/src/main/java/com/pawa/ec/udomidrive_demo/MainActivity.java
+++ app/src/main/java/com/pawa/ec/udomidrive_demo/MainActivity.java
@@ -2,13 +2,44 @@ package com.pawa.ec.udomidrive_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.btn_continue) Button btn_continue;
+    @BindView(R.id.btn_driver) Button btn_driver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+        btn_driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                Intent intent = new Intent(MainActivity.this, MainActivity5.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
