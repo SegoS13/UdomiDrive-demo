8e09c26b873c32282804053280a5a6b48cdd26d8
Mauricio <segoviamz@hotmail.com>
2020-11-26 15:16:18 -0500

    Update Project 70%

--- app/src/main/java/com/pawa/ec/udomidrive_demo/MainActivity.java
+++ app/src/main/java/com/pawa/ec/udomidrive_demo/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, UserLocationActivity.class);
                 //Intent intent = new Intent(MainActivity.this, MainActivity4.class);
+
+
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
